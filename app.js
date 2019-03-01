ff730b8c52435d2c6dce6da4f5413ada4c2a5344
Eduardo Garcia <eddybolivia@gmail.com>
2019-03-01 20:56:42 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+
+// settings
+app.set('port', process.env.PORT || 3000);
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+// middlewares
+app.use((req, res, next) => {
+    console.log(`${req.url} -${req.method}`);
+    next();
+});
+
+// routes
+
+// static files
+
+// start the server
+app.listen(app.get('port'), () => {
+    console.log('Server on port', app.get('port'));
+});
